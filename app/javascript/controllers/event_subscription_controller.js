20907e8c10c5ca7fe8dcf11f71b52f3c9455a84f
stefany santos <stefany.chsantos@gmail.com>
2023-11-28 14:49:53 -0300

    stimulus chat

--- app/javascript/controllers/event_subscription_controller.js
+++ app/javascript/controllers/event_subscription_controller.js
@@ -9,9 +9,18 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel:"EventChannel", id: this.eventIdValue },
-      { received: data => console.log(data)}
+      { received: data =>
+        this.#insertMessageAndScrollDown(data)
+      }
     )
 
     console.log('hello');
+  }
+
+
+
+  #insertMessageAndScrollDown(data){
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 }
