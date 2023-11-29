d1a828b89950ac8b4b349586b1b719e4151186bd
stefany santos <stefany.chsantos@gmail.com>
2023-11-29 10:23:49 -0300

    commit status

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "events#index"
 
   resources :events, only: %i[new create index show delete] do
+    get "chat", to: "events#chat"
     resources :messages, only: %i[new create]
     resources :participants, only: %i[new create]
   end
